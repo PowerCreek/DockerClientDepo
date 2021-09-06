860b2197865d7d781d2babc259ec58b129b04e90
EpicMice <micecd@gmail.com>
2021-09-05 21:12:16 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerClientDepo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("trigger")]
+        public async Task<IActionResult> Trigger([FromServices] ProxyData proxy)
+        {
+            proxy.CurrentHost = "https://localhost:5005";
+            await Task.Delay(3000);
+            proxy.CurrentHost = "https://localhost:5003";
+            return Ok("some stuff");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace DockerClientDepo
+{
+
+    public class TestMiddleware
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly RequestDelegate _nextMiddleware;
+        
+        public TestMiddleware(RequestDelegate nextMiddleware)
+        {
+            _nextMiddleware = nextMiddleware;
+        }
+    }
+    
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ProxyData>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+
+            });
+
+            app.UseMiddleware<ReverseProxyMiddleware>();
+            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("hello world");
+            });
+        }
+    }
+}
